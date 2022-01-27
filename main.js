9dfd142a819bed31d5b4463925dd8cb125799d05
Sarveshraam <90337673+Sarveshraam@users.noreply.github.com>
2022-01-27 19:47:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(640,480)
+canvas.position(110,250)
+video=createCapture(VIDEO)
+video.hide();
+
+
+
+}
+
+function draw(){
+    image(video,200.200,150,400)
+    fill(255,0,0)
+    stroke(255,0,0)
+    circle(50,50,80)
+     circle(50,430,80)
+  circle(590,50,80)
+     circle(590,430,80)
+}
+
+function take_snapshot(){
+    save('student_name_png')
+}
+
